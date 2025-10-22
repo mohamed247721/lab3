91f3f8769cbe7a6ea600ef2562204de0ec59bb17
mohamed247721 <mohamed247721@bue.edu.eg>
2025-10-22 05:16:26 -0700

    lab3

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -53,7 +53,7 @@ public class PlayerController : MonoBehaviour {
                 GetComponent<SpriteRenderer>().flipX = false;
             }   
         }
-         anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+         anim.SetFloat("speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
 anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
 anim.SetBool("Grounded", grounded);
 
